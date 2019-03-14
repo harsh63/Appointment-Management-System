58bb81680998c02851ca00fd8f8e0f96bc8d994d
harsh63 <hsagar63@gmail.com>
2019-03-14 17:08:24 +0530

    rout guard added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :patients
+
+  unauthenticated :patient do
+    root to: 'devise/session#new'
+  end
+
+  authenticated :patient do
+    root to: 'dashboard#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
