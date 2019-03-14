20a308b4fad877f775794b2cc2646541803a463a
harsh63 <hsagar63@gmail.com>
2019-03-14 13:03:05 +0530

    Materialized css added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -11,6 +12,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require materialize
 //= require activestorage
 //= require turbolinks
 //= require_tree .
